da5bffa4116fe8bc20f6389cc1c1f31ab691ed05
hdfm2022 <hdfmacedo+git@gmail.com>
2021-08-19 18:20:41 -0300

    tirando erro do visual studio. mas nao entendi pq.

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(router);
 app.use((err: Error, req, res, next) => {
+    const errmessage = err.message;
     if (err instanceof SyntaxError && 'body' in err) {
-        return res.status(400).send({ status: 400, message: err.message }); // Bad request
+        return res.status(400).send({ status: 400, message: errmessage }); // Bad request
     }
     next();
 });
